734685465ce7c68866909c6686ab2b025804cad1
guoqing.dong <878744510@qq.com>
2020-02-24 21:26:00 +0800

    2

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -17,7 +17,7 @@ module.exports = smart(webpackBase, {
         // 文件名称
         filename: '[name].[contenthash:8].js',
         chunkFilename: '[name].[contenthash:8].js',
-        publicPath: '/',
+        publicPath: '/webpack-demo',
     },
     optimization: {
         minimize: true,
